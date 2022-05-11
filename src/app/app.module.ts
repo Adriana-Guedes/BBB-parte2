367b79ade07919a424a84d1b6b92a7f9e4753b5d
Adriana-Guedes <drikaguedess@gmail.com>
2022-05-11 19:09:27 -0300

    Front end finalizado

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { VotacaoComponent } from './votacao/votacao.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RegistrarVotacaoComponent } from './registrar-votacao/registrar-votacao.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VotacaoComponent,
+    RegistrarVotacaoComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
